3fd153898a1be1f74cb50e1f6a854b781d47717b
Brett Mayer <bmayer55@yahoo.com>
2018-10-04 09:10:24 -0400

    init  commit

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        //....https://csharp.christiannagel.com/2017/01/25/expressionbodiedmembers/
+        public IQueryable<Product> Products => context.Products;
+    }
+}
